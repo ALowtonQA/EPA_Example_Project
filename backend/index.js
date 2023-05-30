6cc069ca264c16a5b01ef6e3b87bdfe7124ca2dd
ALowtonQA <alowton@qa.com>
2023-05-30 14:41:00 +0100

    AL: add routes to app

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 "use strict";
 
 // Setup
+const mongoURL = "mongodb://127.0.0.1:27017/EPA";
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const port = 8080;
-const mongoURL = "mongodb://127.0.0.1:27017/EPA";
+const review_routes = require("./Routes/reviews");
+const user_routes = require("./Routes/users");
 
 // Middleware, Routes & Config
 APP.use(CORS());                       // Disable Cross Origin Resource Sharing Restrictions
 APP.use(EXPRESS.json());              // JSON Body Parser
+APP.use(user_routes);                // Routes for collections
+APP.use(review_routes);             // ""
 
 // Connect to MongoDB
 mongoose
