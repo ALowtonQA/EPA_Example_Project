16c48a0cda3b7405aff3ec7d56ba6832d56a93db
ALowtonQA <alowton@qa.com>
2023-05-30 15:36:37 +0100

    AL: setup state

--- frontend/src/components/Navigation/MainNavBar.js
+++ frontend/src/components/Navigation/MainNavBar.js
@@ -1,4 +1,11 @@
+import { useState } from 'react';
+
+const pages = ['Reviews', 'Create Review'];
+const settings = ['Change Username', 'Change Password', 'Delete Account', 'Logout'];
+
 function MainNavBar() {
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
 
   return (
     <></>
