9b358119bef84775fd437a926d2447e882abad1b
ALowtonQA <alowton@qa.com>
2023-05-30 14:45:16 +0100

    AL: get all reviews

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -2,7 +2,17 @@
 
 // Setup
 const router = require("express").Router();
-const review = require("../models/review");
+const review = require("../Models/review");
+
+// Get Requests (200 is default HTTP status code)
+router.get("/review/getAll", async(req, res, next) => {
+    try {
+        const reviews = await review.find();
+        res.send(reviews);
+    } catch(err) {
+        next(new Error(err.message));
+    }
+});
 
 // Export
 module.exports = router;
