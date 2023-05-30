e65775eefdeb2d6a8da1f39efc58a9bd3874e235
ALowtonQA <alowton@qa.com>
2023-05-30 15:37:08 +0100

    AL: materialUI setup

--- frontend/src/components/Navigation/MainNavBar.js
+++ frontend/src/components/Navigation/MainNavBar.js
@@ -1,11 +1,56 @@
 import { useState } from 'react';
 
+
 const pages = ['Reviews', 'Create Review'];
 const settings = ['Change Username', 'Change Password', 'Delete Account', 'Logout'];
 
 function MainNavBar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
   const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const stringToColor = (string) => {
+    let hash = 0;
+    let i;
+  
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+  
+    return color;
+  }
+
+  const stringAvatar = (name) => {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name[0][0]}`,
+    };
+  }
 
   return (
     <></>
