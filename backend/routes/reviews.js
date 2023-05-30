9f88c8a62ad2fce165b6f2fcc920014011a81759
ALowtonQA <alowton@qa.com>
2023-05-30 14:48:33 +0100

    AL: delete reviews

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -2,7 +2,7 @@
 
 // Setup
 const router = require("express").Router();
-const review = require("../Models/review");
+const review = require("../models/review");
 
 // Get Requests (200 is default HTTP status code)
 router.get("/review/getAll", async(req, res, next) => {
