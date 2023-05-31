b841386eaae592987cb533cc7e646178c50756da
ALowtonQA <alowton@qa.com>
2023-05-31 11:14:29 +0100

    AL: init UAT

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,14 @@
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import Login from './components/User/Login';
+import { BrowserRouter } from 'react-router-dom';
 
 describe("Did pages render correctly test", () => {
 
-  
+  test('Login page rendered correctly', () => {
+    render(<BrowserRouter> <Login /> </BrowserRouter>);
+    const element = screen.getByText("Don't have an account? Sign Up");
+    expect(element).toBeInTheDocument();
+  });
 
 });
