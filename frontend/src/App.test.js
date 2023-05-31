7a75b9577e24aeb945d9916392662b9f275e3fb1
ALowtonQA <alowton@qa.com>
2023-05-31 11:15:04 +0100

    AL: UAT signup

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,12 +2,19 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Login from './components/User/Login';
 import { BrowserRouter } from 'react-router-dom';
+import SignUp from './components/User/Signup';
 
 describe("Did pages render correctly test", () => {
 
   test('Login page rendered correctly', () => {
     render(<BrowserRouter> <Login /> </BrowserRouter>);
     const element = screen.getByText("Don't have an account? Sign Up");
+    expect(element).toBeInTheDocument();
+  });
+  
+  test('Login page rendered correctly', () => {
+    render(<BrowserRouter> <SignUp /> </BrowserRouter>);
+    const element = screen.getByText("Already have an account? Sign in");
     expect(element).toBeInTheDocument();
   });
 
