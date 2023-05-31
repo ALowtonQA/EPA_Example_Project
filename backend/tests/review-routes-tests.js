98a49e32574848cc00d49fb5374e77b8b6c0ce5d
ALowtonQA <alowton@qa.com>
2023-05-31 10:48:20 +0100

    AL: init review route test

---
+++ backend/tests/review-routes-tests.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const http = require('chai-http');
+chai.use(http);
+const app = require('../index');
+const expect = chai.expect;
+
+describe('Testing the review routes', function() {
+
+    
+
+});
