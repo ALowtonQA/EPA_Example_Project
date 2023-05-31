51ead971d36a88ab519378f6ddc8cff2e2f0243a
ALowtonQA <alowton@qa.com>
2023-05-31 10:49:14 +0100

    AL: review route put

--- backend/tests/review-routes-tests.js
+++ backend/tests/review-routes-tests.js
@@ -6,6 +6,8 @@ const expect = chai.expect;
 
 describe('Testing the review routes', function() {
 
+    let result;
+
     it('Should complete post request with status 201', function(done) {
         chai.request(app)
             .post('/review/create')
@@ -20,6 +22,23 @@ describe('Testing the review routes', function() {
                 expect(err).to.be.null;
                 expect(response).to.have.status(201);
                 result = response.body;
+                done();
+            });
+    });
+
+    it('Should complete put request with status 200', function(done) {
+        chai.request(app)
+            .put(`/review/update/${result._id}`)
+            .send({
+                "book_title": "MochaChaiTestUpdate", 
+                "body": "MochaChaiTestUpdate",
+                "author": "MochaChaiTestUpdate",
+                "user": "MochaChaiTestUpdate",
+                "date": new Date()
+            })
+            .end(function(err, response) {
+                expect(err).to.be.null;
+                expect(response).to.have.status(200);
                 done();
             });
     });
