1948eeea95f919db265fac29f5895b2977092306
ALowtonQA <alowton@qa.com>
2023-05-31 10:52:41 +0100

    AL: user route post

--- backend/tests/user-routes-tests.js
+++ backend/tests/user-routes-tests.js
@@ -6,5 +6,18 @@ const expect = chai.expect;
 
 describe('Testing the user routes', function() {
 
-    
+    it('Should complete post request with status 201', function(done) {
+        chai.request(app)
+            .post('/user/register')
+            .send({
+                "username": "MochaChaiTest", 
+                "password": "Test"
+            })
+            .end(function(err, response) {
+                expect(err).to.be.null;
+                expect(response).to.have.status(201);
+                done();
+            });
+    });
+
 });
