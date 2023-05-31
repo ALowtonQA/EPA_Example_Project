89e415e77b37e8717db764f3e228a9d1153509e1
ALowtonQA <alowton@qa.com>
2023-05-31 11:15:33 +0100

    AL: UAT reviews

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom';
 import Login from './components/User/Login';
 import { BrowserRouter } from 'react-router-dom';
 import SignUp from './components/User/Signup';
+import Reviews from './components/Reviews/Reviews';
 
 describe("Did pages render correctly test", () => {
 
@@ -15,6 +16,13 @@ describe("Did pages render correctly test", () => {
   test('Login page rendered correctly', () => {
     render(<BrowserRouter> <SignUp /> </BrowserRouter>);
     const element = screen.getByText("Already have an account? Sign in");
+    expect(element).toBeInTheDocument();
+  });
+
+  test('Login page rendered correctly', () => {
+    sessionStorage.setItem("username", "Test")
+    render(<BrowserRouter> <Reviews /> </BrowserRouter>);
+    const element = screen.getByText("Write a Review");
     expect(element).toBeInTheDocument();
   });
 
